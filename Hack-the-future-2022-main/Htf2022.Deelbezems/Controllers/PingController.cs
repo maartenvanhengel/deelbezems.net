67e43ba71dfe6afeb0d0320438b0a5c676103b7a
maartenvanhengel <maartenvanhengel2001@gmail.com>
2022-11-15 10:35:36 +0100

    challange 1

---
+++ Hack-the-future-2022-main/Htf2022.Deelbezems/Controllers/PingController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Htf2022.Deelbezems.Controllers
+{
+    [Route("ping")]
+    [ApiController]
+    public class PingController : ControllerBase
+    {
+        [HttpGetAttribute]
+        public IActionResult Index()
+        {
+            return Ok("pong");
+        }
+    }
+}
